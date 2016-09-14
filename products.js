51b1cb254a006ed4d5528c82832107537d934ae4
Khaled <Khaled@nowhere.com>
2016-09-15 00:56:46 +0200

    main.html book content section now get content dynamically from product.js

---
+++ products.js
@@ -0,0 +1,74 @@
+var products = [
+  {
+    id: "the-great-gatsby",
+    name: "The Great Gatsby	",
+    category: "books",
+    description: "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.",
+    price: 25,
+    picture_url: "images/greatgatsby.jpg"
+  },
+  {
+    id: "the-grapes-of-wrath",
+    name: "The Grapes Of Wrath",
+    category: "books",
+    description: "To the red country and part of the gray country of Oklahoma, the last rains came gently, and they did not cut the scarred earth",
+    price: 9.99,
+    picture_url: "images/thegrapesofwrath.jpg"
+  },
+  {
+    id: "1984",
+    name: "1984",
+    category: "books",
+    description: "It was a bright cold day in April, and the clocks were striking thirteen",
+    price: 8,
+    picture_url: "images/1984.jpg"
+  },
+  {
+    id: "ulysses",
+    name: "Ulysses",
+    category: "books",
+    description: "Stately, plump Buck Mulligan came from the stairhead, bearing a bowl of lather on which a mirror and a razor lay crossed.",
+    price: 8,
+    picture_url: "images/ulysses.jpg"
+  },
+  {
+    id: "lolita",
+    name: "Lolita",
+    category: "books",
+    description: "Lolita, light of my life, fire of my loins. My sin, my soul. Lo-lee-ta: the tip of the tongue taking a trip of three steps down the palette to tap, at three, on the teeth",
+    price: 8,
+    picture_url: "images/lolita.jpg"
+  },
+  {
+    id: "catch22",
+    name: "Catch22",
+    category: "books",
+    description: "It was love at first sight.",
+    price: 8,
+    picture_url: "images/catch22.jpg"
+  },
+  {
+    id: "",
+    name: "Dark Side of the Moon",
+    category: "music",
+    description: ["Collector's Edition", "Trippy", "Good for late night existential crises"],
+    price: 100,
+    picture_url: "http://i.kinja-img.com/gawker-media/image/upload/s--9N1Ijk1t--/c_fit,fl_progressive,q_80,w_636/1940ob66cyu2ljpg.jpg"
+  },
+  {
+    id: "",
+    name: "Thriller",
+    category: "music",
+    description: ["Classic MJ", "The 80's greatest contribution to human progress"],
+    price: 19,
+    picture_url: "http://cps-static.rovicorp.com/3/JPG_400/MI0000/677/MI0000677650.jpg"
+  },
+  {
+    id: "",
+    name: "Ella & Luis",
+    category: "music",
+    description: ["How could you not?", "Seriously, though..."],
+    price: 110,
+    picture_url: "http://ecx.images-amazon.com/images/I/51713fx1VdL._SY300_.jpg"
+  }
+]
