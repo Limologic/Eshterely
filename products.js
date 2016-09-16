ddf9183a07c73a03edd8943487358d31849e7644
Khaled <Khaled@nowhere.com>
2016-09-16 15:01:37 +0200

    All content & events in MAIN page is now dynamic

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ var products = [
   {
     id: "the-great-gatsby",
     name: "The Great Gatsby	",
-    category: "books",
+    category: "Books",
     description: "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.",
     price: 25,
     picture_url: "images/greatgatsby.jpg"
@@ -10,7 +10,7 @@ var products = [
   {
     id: "the-grapes-of-wrath",
     name: "The Grapes Of Wrath",
-    category: "books",
+    category: "Books",
     description: "To the red country and part of the gray country of Oklahoma, the last rains came gently, and they did not cut the scarred earth",
     price: 9.99,
     picture_url: "images/thegrapesofwrath.jpg"
@@ -18,7 +18,7 @@ var products = [
   {
     id: "1984",
     name: "1984",
-    category: "books",
+    category: "Books",
     description: "It was a bright cold day in April, and the clocks were striking thirteen",
     price: 8,
     picture_url: "images/1984.jpg"
@@ -26,7 +26,7 @@ var products = [
   {
     id: "ulysses",
     name: "Ulysses",
-    category: "books",
+    category: "Books",
     description: "Stately, plump Buck Mulligan came from the stairhead, bearing a bowl of lather on which a mirror and a razor lay crossed.",
     price: 8,
     picture_url: "images/ulysses.jpg"
@@ -34,7 +34,7 @@ var products = [
   {
     id: "lolita",
     name: "Lolita",
-    category: "books",
+    category: "Books",
     description: "Lolita, light of my life, fire of my loins. My sin, my soul. Lo-lee-ta: the tip of the tongue taking a trip of three steps down the palette to tap, at three, on the teeth",
     price: 8,
     picture_url: "images/lolita.jpg"
@@ -42,7 +42,7 @@ var products = [
   {
     id: "catch22",
     name: "Catch22",
-    category: "books",
+    category: "Books",
     description: "It was love at first sight.",
     price: 8,
     picture_url: "images/catch22.jpg"
@@ -50,7 +50,7 @@ var products = [
   {
     id: "dark",
     name: "Dark Side of the Moon",
-    category: "music",
+    category: "Music",
     description: ["Collector's Edition", "Trippy", "Good for late night existential crises"],
     price: 100,
     picture_url: "http://i.kinja-img.com/gawker-media/image/upload/s--9N1Ijk1t--/c_fit,fl_progressive,q_80,w_636/1940ob66cyu2ljpg.jpg"
@@ -58,7 +58,7 @@ var products = [
   {
     id: "thriller",
     name: "Thriller",
-    category: "music",
+    category: "Music",
     description: ["Classic MJ", "The 80's greatest contribution to human progress"],
     price: 19,
     picture_url: "http://cps-static.rovicorp.com/3/JPG_400/MI0000/677/MI0000677650.jpg"
@@ -66,7 +66,7 @@ var products = [
   {
     id: "ella",
     name: "Ella & Luis",
-    category: "music",
+    category: "Music",
     description: ["How could you not?", "Seriously, though..."],
     price: 110,
     picture_url: "http://ecx.images-amazon.com/images/I/51713fx1VdL._SY300_.jpg"
@@ -74,7 +74,7 @@ var products = [
   {
     id: "pacman",
     name: "Pac man",
-    category: "games",
+    category: "Games",
     description: "Pac-Man is an arcade game developed by Namco and first released in Japan in May 1980. It was created by Japanese video game designer Toru Iwatani. It was licensed for distribution in the United States by Midway and released in October 1980.",
     price: 110,
     picture_url: "images/pacman.jpg"
@@ -82,7 +82,7 @@ var products = [
   {
     id: "digdug",
     name: "Dig Dug",
-    category: "games",
+    category: "Games",
     description: "Dig Dug is an arcade game developed and published by Namco in Japan in 1982. It runs on Namco Galaga hardware, and was published outside Japan by Atari, Inc..",
     price: 110,
     picture_url: "images/digdug.png"
@@ -90,7 +90,7 @@ var products = [
   {
     id: "bioshock",
     name: "BioShock",
-    category: "games",
+    category: "Games",
     description: "BioShock is a first-person shooter video game developed by 2K Boston and 2K Australia, and published by 2K Games.",
     price: 110,
     picture_url: "images/bioshock.jpg"
@@ -98,7 +98,7 @@ var products = [
   {
     id: "frogger",
     name: "Frogger",
-    category: "games",
+    category: "Games",
     description: "Frogger is a 1981 arcade game developed by Konami and licensed for North American distribution by Sega-Gremlin and worldwide by Sega itself.",
     price: 110,
     picture_url: "images/frogger.jpg"
