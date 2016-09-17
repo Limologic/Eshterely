980aa17bddcbbf709dcbbcba6da65f90be0d2ebf
Khaled <Khaled@nowhere.com>
2016-09-17 14:07:28 +0200

    fix cart count on details page and main page

--- my-html-loader.js
+++ my-html-loader.js
@@ -1,5 +1,8 @@
-var loader= function(filename, where){
+var loader= function(filename, where, callback){
   $.get(filename).success(function(resp) {
     $(where).html(resp)
+    if(callback){
+      callback();
+    }
   })
 }
